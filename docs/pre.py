85fad97ebcd5e9dc0e63d692d9ef3cba9d5b17e2
Dimen <dimenerno@kaist.ac.kr>
2025-02-20 16:53:41 +0900

    Rendered ramsey

--- docs/pre.py
+++ docs/pre.py
@@ -14,16 +14,16 @@ def process_markdown_file(filename):
     with open(file_path, 'r', encoding='utf-8') as file:
         content = file.read()
 
-    # 1. $$로 감싸진 내용에 대해, $$ 위아래로 여백이 한 칸 있어야 하고 내용은 $$와 다른 줄에 있어야 함
-    def format_dollar_dollar(match):
-        inner_content = match.group(1).strip()
-        # $$ 위 아래로 여백이 없으면 여백을 추가하고 줄바꿈을 함
-        if not inner_content.startswith("\n") and not inner_content.endswith("\n"):
-            return f'\n$$\n{inner_content}\n$$\n'
-        return match.group(0)  # 이미 여백이 있을 경우는 변경하지 않음
+    # # 1. $$로 감싸진 내용에 대해, $$ 위아래로 여백이 한 칸 있어야 하고 내용은 $$와 다른 줄에 있어야 함
+    # def format_dollar_dollar(match):
+    #     inner_content = match.group(1).strip()
+    #     # $$ 위 아래로 여백이 없으면 여백을 추가하고 줄바꿈을 함
+    #     if not inner_content.startswith("\n") and not inner_content.endswith("\n"):
+    #         return f'\n$$\n{inner_content}\n$$\n'
+    #     return match.group(0)  # 이미 여백이 있을 경우는 변경하지 않음
 
-    # $$로 감싸진 부분을 찾아서 여백 추가하기
-    content = re.sub(r'\$\$(.*?)\$\$', format_dollar_dollar, content, flags=re.DOTALL)
+    # # $$로 감싸진 부분을 찾아서 여백 추가하기
+    # content = re.sub(r'\$\$(.*?)\$\$', format_dollar_dollar, content, flags=re.DOTALL)
 
     # 2. $로 감싸진 내용에 대해, 특수 문자를 변경
     def escape_special_chars(match):
