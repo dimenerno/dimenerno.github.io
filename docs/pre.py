b4777264cd02afe26fd3c3076d2335c9906646b4
dimenerno <dimenerno@kaist.ac.kr>
2025-05-08 11:07:01 +0900

    Added robinson-craig

--- docs/pre.py
+++ docs/pre.py
@@ -42,5 +42,5 @@ def process_markdown(filename):
 
 if __name__ == "__main__":
     # filename = input("수정 파일: ")
-    filename = "2025-05-07-robinson-craig-beth"
+    filename = "2025-05-08-robinson-craig"
     process_markdown(filename)
