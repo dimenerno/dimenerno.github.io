72641ba9cd3fa6200f39ae1e28ea36abb347f80b
Dimen <dimenerno@kaist.ac.kr>
2025-07-07 00:25:10 +0900

    Added some drafts

--- pre.py
+++ pre.py
@@ -42,5 +42,5 @@ def process_markdown(filename):
 
 if __name__ == "__main__":
     # filename = input("수정 파일: ")
-    filename = "2024-12-01-baire-en"
+    filename = "2025-07-06-stone-representation"
     process_markdown(filename)
