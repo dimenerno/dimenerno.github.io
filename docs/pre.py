2337bf9d65a3153d096107c1c5fd09dae1a62712
dimenerno <dimenerno@kaist.ac.kr>
2025-07-08 16:00:32 +0900

    Added urysohn

--- docs/pre.py
+++ docs/pre.py
@@ -42,5 +42,5 @@ def process_markdown(filename):
 
 if __name__ == "__main__":
     # filename = input("수정 파일: ")
-    filename = "2025-07-08-urysohn-en"
+    filename = "2025-07-08-urysohn"
     process_markdown(filename)
