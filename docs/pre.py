7f83834eee0a051aec60c5f18eb0ec9eed4f1ed5
dimenerno <dimenerno@kaist.ac.kr>
2025-08-06 08:11:21 +0900

    Added some pdf files

--- docs/pre.py
+++ docs/pre.py
@@ -42,5 +42,5 @@ def process_markdown(filename):
 
 if __name__ == "__main__":
     # filename = input("수정 파일: ")
-    filename = "2025-07-18-boolean-algebra"
+    filename = "2025-08-01-compactness-los"
     process_markdown(filename)
