abcc5239228a7d9fc8fb301ac4c55eedc88a6887
dimenerno <dimenerno@kaist.ac.kr>
2025-09-25 15:59:10 +0900

    Added ch-for-borel

--- docs/pre.py
+++ docs/pre.py
@@ -42,5 +42,5 @@ def process_markdown(filename):
 
 if __name__ == "__main__":
     # filename = input("수정 파일: ")
-    filename = "2025-09-16-cardinal-arithmetics"
+    filename = "2025-09-23-ch-for-borel"
     process_markdown(filename)
